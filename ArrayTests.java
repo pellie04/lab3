57faec9e5142085cc70dbfed62958e04220d3ebf
pellie04 <pellie@ucsd.edu>
2022-10-13 14:45:07 -0700

    fix array

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -15,4 +18,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedInPlaceNew(){
+    int[] input1 = { 3 , 4 , 5 , 6};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 6, 5, 4, 3 }, input1);
+  }
+
+  @Test
+  public void testReversedNew(){
+    int[] input1 = { 3 , 4 , 5 , 6};
+    assertArrayEquals(new int[]{ 6, 5 ,4, 3}, ArrayExamples.reversed(input1));
+    }
 }
